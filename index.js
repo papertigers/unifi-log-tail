e7ca48c1dc20a0b44af3e0dfd1cf0a6a383eb063
Mike Zeller <mike@mikezeller.net>
2017-03-20 06:05:39 +0000

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+var rdbpool = require('rethinkdb-pool');
+var r = require('rethinkdb');
+var fs = require('fs');
+var Tail = require('tail').Tail;
+var parse = require('unifi-video-log-parser');
+
+var caCert = fs.readFileSync(__dirname + '/cacert');
+
+var database_config = {
+	host: 'rethinkdb',
+	user: 'admin',
+	password: '',
+	db: 'unifi',
+	max: 5,
+	ssl: {
+		rejectUnauthorized: false,
+		ca: caCert
+	}
+};
+
+var pool = new rdbpool(database_config);
+
+tail = new Tail('/var/log/unifi-video/recording.log');
+
+tail.on("line", function lineEvent(line) {
+	var event = parse(line);
+	var query = r.table('events').insert(event);
+
+	pool.run(query, function(err, cursor) {
+		if (err) throw err;
+	});
+});
+
+tail.on("error", function(error) {
+	throw error;
+});
